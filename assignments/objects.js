fdb8b16e91378d2a930cc95bc2f8768a41704eca
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-07-22 14:39:09 -0500

    finished object stretch, starting on arrays

--- assignments/objects.js
+++ assignments/objects.js
@@ -89,22 +89,36 @@ console.log(Antonietta.multiplyNums(3,4));
 const parent = {
   name: "Susan",
   age: 70,
+  speak:  function() {
+   return this.name;
+  },
   child: {
-    name: George,
+    name: "George",
     age: 50,
+    speak: function() {
+      return this.name
+    },
+    grandchild: {
+      name: "Sam",
+      age: 30,
+      speak: function() {
+        return this.name
+      },
+    }
   }
 
 
 }
 
 // Log the parent object's name
-
+console.log(parent.name)
 // Log the child's age
-
+console.log(parent.child.age);
 // Log the name and age of the grandchild
-
+console.log(parent.child.grandchild.name, parent.child.grandchild.age );
 // Have the parent speak
-
+console.log(parent.speak());
 // Have the child speak
-
+console.log(parent.child.speak());
 // Have the grandchild speak
+console.log(parent.child.grandchild.speak());
